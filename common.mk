6a335bcfc66e4e3c259f901f5f28c8e46c833537
Roshan Pius <rpius@google.com>
2020-04-24 14:33:28 -0700

    sm8150-common: Add WifiOverlay
    Bug: 148617260
    Test: Device boots up and connects to wifi networks.
    Change-Id: Iec27a6d947a232a42451ef095dc73d1a52327db5

--- common.mk
+++ common.mk
@@ -517,6 +517,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifi.supplicant@2.0.vendor:64 \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor:64 \
     wifi-mac-generator \
+    WifiOverlay \
     wpa_supplicant \
     wpa_supplicant.conf
 
