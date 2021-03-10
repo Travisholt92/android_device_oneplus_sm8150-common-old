yaap/device_oneplus_sm8150-common

sm8150-common: re-enable enforcing RROs
* partially revert d16a0177f808fa32a6a1cfc255f143f2b3eb3236

Signed-off-by: Omkar Chandorkar <gotenksIN@aosip.dev>

common.mk
@@ -16,7 +16,13 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-system \
     $(LOCAL_PATH)/overlay-yaap
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-system
 
 # A/B
 AB_OTA_UPDATER := true

Travisholt92/android_device_oneplus_sm8150-common-old

common.mk
@@ -19,7 +19,13 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-kangos
+    $(LOCAL_PATH)/overlay-kangos \
+    $(LOCAL_PATH)/overlay-system
+
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-system
 
 # A/B
 AB_OTA_UPDATER := true